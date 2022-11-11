2a3b16902825e1e8cc091a5ee9abdf0f4042021d
klelia <clelia.fradella@gmail.com>
2022-11-11 11:42:49 +0100

    product component

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col">
+        <img src="https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg" alt="">
+        <h4>N.38 Mezze maniche</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
